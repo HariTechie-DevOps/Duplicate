9516df1fdf983cfc69122b64ea8bdaa4c38d183b
Harisankar <harisankar2206@gmail.com>
2025-12-29 14:35:55 +0530

    Delete duplicate/src/main/java/com/spark/chat/dto directory

--- duplicate/src/main/java/com/spark/chat/dto/SignupRequest.java
+++
@@ -1,9 +0,0 @@
-package com.spark.chat.dto;
-
-public class SignupRequest {
-    public String name;
-    public String mobile;
-    public String password;
-    public int age;
-    public String gender;
-}
