633fd0e3a1d3a82ea35bbc27b373999122866729
Harisankar <harisankar2206@gmail.com>
2025-12-30 20:02:02 +0530

    Update DemoApplication.java

--- duplicate/src/main/java/com/spark/chat/DemoApplication.java
+++ duplicate/src/main/java/com/spark/chat/DemoApplication.java
@@ -1,13 +1,10 @@
 package com.spark.chat;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
-        // This starts the embedded Tomcat server on port 8080
+        
         SpringApplication.run(DemoApplication.class, args);
-        System.out.println("Backend is running! You can now open your HTML files.");
     }
 }
+
