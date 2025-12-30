9a21e91bf991d1790db2b602eab855cf216480f9
Harisankar <harisankar2206@gmail.com>
2025-12-30 20:11:51 +0530

    Update and rename UserRepository.java to signuprepository.java

---
+++ duplicate/src/main/java/com/spark/chat/repository/signuprepository.java
@@ -0,0 +1,10 @@
+package com.spark.chat.repository;
+
+import com.spark.chat.repository.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface signuprepository extends JpaRepository<User, Long> {
+   
+}
+
