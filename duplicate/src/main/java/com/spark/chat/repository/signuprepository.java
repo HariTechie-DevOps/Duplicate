84e615a6ac3a7354dc8d7ae1e1d347aee1119bfe
Harisankar <harisankar2206@gmail.com>
2025-12-30 21:32:54 +0530

    Update signuprepository.java

--- duplicate/src/main/java/com/spark/chat/repository/signuprepository.java
+++ duplicate/src/main/java/com/spark/chat/repository/signuprepository.java
@@ -1,6 +1,6 @@
 package com.spark.chat.repository;
 import java.util.List;
-
+import com.spark.chat.entity.User;
 import com.spark.chat.repository.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface signuprepository extends JpaRepository<User, Long> {
    List<user> findByName(String name);
 }
+
 
 
 
