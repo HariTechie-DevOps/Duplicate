ad842336a2730e7e7f41ab44f642c7e34927e335
Harisankar <harisankar2206@gmail.com>
2025-12-30 21:17:33 +0530

    Update signupresponse.java

--- duplicate/src/main/java/com/spark/chat/dto/signupresponse.java
+++ duplicate/src/main/java/com/spark/chat/dto/signupresponse.java
@@ -2,7 +2,7 @@ class signupresponse {
   boolean field;
   String message;
 
-  public signuprequest(boolean field,String message) {
+  public signupresponse(boolean field,String message) {
     this.field = field;
     this.message = message;
   }
