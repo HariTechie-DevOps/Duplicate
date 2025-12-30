b3280113c494a33915311801acdcfd756cef1537
Harisankar <harisankar2206@gmail.com>
2025-12-30 20:07:50 +0530

    Update and rename User.java to user.java

--- duplicate/src/main/java/com/spark/chat/entity/user.java
+++ duplicate/src/main/java/com/spark/chat/entity/user.java
@@ -1,23 +1,22 @@
 package com.spark.chat.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
 @Entity 
 @Data 
 @Table(name = "users")
-public class User {
+public class user {
+    
     @Id 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
     private String name;
     private Integer age;
     private String gender;
     
-    @Column(unique = true) // Ensures Nth user mobile uniqueness
+    @Column(unique = true) 
     private String mobile;
-    
     private String password;
-    private String token; // For Remember Me functionality
+    private String token; 
 }
+
