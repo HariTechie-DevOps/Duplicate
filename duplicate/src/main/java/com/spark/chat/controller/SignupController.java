37a836cb4d5fbde070aa1c645463831d19e2d5e6
Harisankar <harisankar2206@gmail.com>
2025-12-30 21:59:39 +0530

    Update and rename signupcontroller.java to SignupController.java

---
+++ duplicate/src/main/java/com/spark/chat/controller/SignupController.java
@@ -0,0 +1,22 @@
+package com.spark.chat.controller;
+
+import com.spark.chat.dto.SignupRequest;
+import com.spark.chat.dto.SignupResponse;
+import com.spark.chat.services.SignupService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "*") 
+public class SignupController {
+
+    private final SignupService service;
+
+    public SignupController(SignupService service){
+        this.service = service;
+    }
+    
+    @PostMapping("/api/signup")
+    public SignupResponse signup(@RequestBody SignupRequest request) {
+        return service.handlesignup(request);
+    }
+}
