437961d93ddb15a6f71f5de29a5a86af8bb8e600
Harisankar <harisankar2206@gmail.com>
2025-12-30 21:57:24 +0530

    Update signuprequest.java

--- duplicate/src/main/java/com/spark/chat/dto/signuprequest.java
+++ duplicate/src/main/java/com/spark/chat/dto/signuprequest.java
@@ -1,7 +1,12 @@
-class signuprequest {
-  String name;
-  int age;
-  String gender;
-  String mobile;
-  String password;
+package com.spark.chat.dto;
+import lombok.Data;
+
+@Data
+public class SignupRequest {
+    private String name;
+    private int age;
+    private String gender;
+    private String mobile;
+    private String password;
 }
+
