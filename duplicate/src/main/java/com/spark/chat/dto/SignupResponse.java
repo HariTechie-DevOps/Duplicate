9f717cef3570d01cd70f9118e923a44b1566420c
Harisankar <harisankar2206@gmail.com>
2025-12-31 21:46:09 +0530

    Update SignupResponse.java

--- duplicate/src/main/java/com/spark/chat/dto/SignupResponse.java
+++ duplicate/src/main/java/com/spark/chat/dto/SignupResponse.java
@@ -1,15 +1,15 @@
 package com.spark.chat.dto;
 import lombok.Data;
+import lombok.NoArgsConstructor; // Add this
+import lombok.AllArgsConstructor;
 
 @Data
+@NoArgsConstructor // Required for JSON tools
+@AllArgsConstructor
 public class SignupResponse {
     private boolean success;
     private String field;
     private String message;
 
-    public SignupResponse(boolean success, String field, String message) {
-        this.success = success;
-        this.field = field;
-        this.message = message;
-    }
+   
 }
