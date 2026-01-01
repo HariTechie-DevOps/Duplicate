1a67c96b39665bc7e0271706238057c0dc395514
Harisankar <harisankar2206@gmail.com>
2026-01-01 15:39:39 +0530

    Update SignupResponse.java

--- duplicate/src/main/java/com/spark/chat/dto/SignupResponse.java
+++ duplicate/src/main/java/com/spark/chat/dto/SignupResponse.java
@@ -6,11 +6,13 @@ public class SignupResponse {
     private boolean success;
     private String field;
     private String message;
+    private String token;
 
-    public SignupResponse(boolean success,String field,String message){
+    public SignupResponse(boolean success,String field,String message,String token){
         this.success = success;
         this.field = field;
         this.message = message;
+        this.token = token;
     }
    
 }
