0770f59ac0fa78e0b77b415d0fc4203750b66132
Harisankar <harisankar2206@gmail.com>
2026-01-03 10:41:56 +0530

    Update SignupRequest.java

--- duplicate/src/main/java/com/spark/chat/dto/SignupRequest.java
+++ duplicate/src/main/java/com/spark/chat/dto/SignupRequest.java
@@ -7,8 +7,13 @@ public class SignupRequest {
 
     // Manually add these for now to fix the build immediately:
     public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
     public String getMobile() { return mobile; }
+    public void setMobile(String mobile) { this.mobile = mobile; }
     public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
     public String getGender() { return gender; }
+    public void setGender(String gender) { this.gender = gender; }
     public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
